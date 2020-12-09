f8e8378f2199e53930ef1862ab38c822acf25ef7
serendipitydt <daimingyu134647@163.com>
2020-12-09 20:26:08 +0800

    第三章结束

--- src/main/java/org/damein/miaosha/Controller/GoodsController.java
+++ src/main/java/org/damein/miaosha/Controller/GoodsController.java
@@ -2,24 +2,17 @@ package org.damein.miaosha.Controller;
 
 import org.damein.miaosha.domain.MiaoshaUser;
 import org.damein.miaosha.redis.RedisService;
-import org.damein.miaosha.result.CodeMsg;
-import org.damein.miaosha.result.Result;
+import org.damein.miaosha.service.GoodsService;
 import org.damein.miaosha.service.MiaoshaUserService;
-import org.damein.miaosha.util.ValidatorUtil;
-import org.damein.miaosha.vo.LoginVo;
+import org.damein.miaosha.vo.GoodsVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.thymeleaf.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
+import java.util.List;
 
 
 @Controller
@@ -35,26 +28,49 @@ public class GoodsController {
     @Autowired
     RedisService redisService;
 
-    //@ResponseBody
+    @Autowired
+    GoodsService goodsService;
+
     @RequestMapping("/to_list")
     //返回的页面的名称，即.html
     public String list(Model model, MiaoshaUser user) {
         model.addAttribute("user", user);
+        //查询商品列表
+        List<GoodsVo> goodsList = goodsService.listGoodsVo();
+        //将参数传到页面上
+        model.addAttribute("goodsList", goodsList);
         return "goods_list";
     }
 
-//    @RequestMapping("/to_detail")
-//    //返回的页面的名称，即.html
-//    public String detail(HttpServletResponse response,Model model,
-//                          @CookieValue(value = MiaoshaUserService.COOKIE_NAME_TOKEN,required = false) String cookieToken,
-//                          @RequestParam(value = MiaoshaUserService.COOKIE_NAME_TOKEN,required = false) String paraToken) {
-//        //model.addAllAttributes("user",new MiaoshaUser());
-//        if (StringUtils.isEmpty(cookieToken) && StringUtils.isEmpty(paraToken)){
-//            return "login";
-//        }
-//        String token=StringUtils.isEmpty(paraToken)?cookieToken:paraToken;//设定优先级
-//        MiaoshaUser user=userService.getByToken(response,token);
-//        model.addAttribute("user",user);
-//        return "goods_list";
-//    }
+    @RequestMapping("/to_detail/{goodsId}")
+    //返回的页面的名称，即.html
+    public String detail(Model model, MiaoshaUser user,
+                         @PathVariable("goodsId") long goodsId) {
+        //自增id不会用普通的自增，要用snowflake算法
+        model.addAttribute("user", user);
+
+        GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
+        model.addAttribute("goods", goods);
+        //
+        long startAt = goods.getStartDate().getTime();
+        long endAt = goods.getEndDate().getTime();
+        long now = System.currentTimeMillis();
+
+        int miaoshaStatus = 0;
+        int remainSeconds=0;
+        if (now < startAt) {//秒杀没开始，倒计时
+            miaoshaStatus=0;
+            remainSeconds=(int)((startAt-now)/1000);
+        } else if (now > endAt) {//秒杀已经结束
+            miaoshaStatus=2;
+            remainSeconds=-1;
+        } else {//秒杀进行中
+            miaoshaStatus=1;
+            remainSeconds=0;
+        }
+        //讲这些参数传到页面上
+        model.addAttribute("miaoshaStatus", miaoshaStatus);
+        model.addAttribute("remainSeconds", remainSeconds);
+        return "goods_detail";
+    }
 }
